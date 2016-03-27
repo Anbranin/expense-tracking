97fa870ace9e156b0a60cc9c7d9274529a24648b
Karin Eichelman <keichelman@gmail.com>
2016-03-26 20:57:57 -0400

    generated reckoning controller

---
+++ app/controllers/reckoning_controller.rb
@@ -0,0 +1,8 @@
+class ReckoningController < ApplicationController
+  def show
+    # TODO: renders a partial. a month? Need a calendar. shit.
+  end
+
+  def index
+  end
+end
---
+++ app/helpers/reckoning_helper.rb
@@ -0,0 +1,2 @@
+module ReckoningHelper
+end
