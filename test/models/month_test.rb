1c24e34a0152a0eae708c5cd24f43488f1cf708c
Karin Eichelman <keichelman@gmail.com>
2016-04-09 20:04:57 -0400

    working tests

--- test/models/month_test.rb
+++ test/models/month_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class MonthTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'month attributes must not be empty' do
+    month = Month.new
+    assert month.invalid?
+    assert month.errors[:name].any?
+    assert month.errors[:days].any?
+  end
+
+  test 'months cannot have days less than 29' do
+    month = Month.new(name: 'February',
+                      days: 20)
+    assert month.invalid?
+    month.days = 29
+    assert month.valid?
+  end
 end
